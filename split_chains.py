11c243f2d1c638eb8211040723dc68e18c148c0d
AnissaMdahoma <49160981+AnissaMdahoma@users.noreply.github.com>
2020-12-07 11:41:30 +0100

    Update split_chains.py

--- split_chains.py
+++ split_chains.py
@@ -11,7 +11,6 @@ def parser_input():
 	parser = argparse.ArgumentParser(
 		description='Prend en entrée un fichier PDB, et le sépare en plusieurs pour chaque chaine.')
 	parser.add_argument("-f",action="store",dest="f",type= str,help = "donnez le fichier pdb",required = True)
-	parser.add_argument("-o",action="store",dest="o",type=str,help ="donnez le nom du fichiers de sortie",required = True)
 
 
 	return (parser)
@@ -20,23 +19,19 @@ def split_chains(df):
 	chaines = np.unique(df["chain_identifier"])
 	for chain in chaines :
 		selection = select_atoms(df,selector={"chain_identifier":[chain]})
-		write_pdb(selection,args.o)
+		write_pdb(selection,'chaine'+chain+'.pdb')
 
 
 def main():
     """Main fonction"""
     df = read_pdb(args.f)
     split_chains(df)
-	#write_pdb(df,args.o)
-    #selection=select_atoms(df,selector={'atom_name':['CA','CB'],'chain_identifier':['A']})
 
 
 if __name__ == '__main__':
 	parser=parser_input()
 	args = parser.parse_args()
 	print('Input file = {}'.format(args.f))
-	print('Input file = {}'.format(args.o))
-	print('Input file = {}'.format(args.o))
 	main()
 
 
